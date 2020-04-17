d199817a1734c78e556e9892b1562e421716ba2d
koberlag <koberlag@gmail.com>
2020-04-17 13:54:00 -0500

    refactored document reading and text extraction

--- project2/main.py
+++ project2/main.py
@@ -4,24 +4,18 @@ import os
 import random
 
 # Max number of files to read for clustering and summarizing
-MAX_FILE_COUNT = 10000
+MAX_FILE_COUNT = 5000
 
 # Directory containing sample of json files for clustering and summarizing
 DIR_TO_READ = "CORD-19-research-challenge/comm_use_subset/comm_use_subset/pdf_json/"
 
-def get_body_text(data):
-    """Extracts the text from the 'body_text' property
-        of the parsed json object."""
-    body_text = data.body_text
-    return body_text
-
-def read_json_files():
-    """Reads json files from the directory given in the 'DIR_TO_RUN" global variable.
+def get_json_file_paths():
+    """Uses the 'DIR_TO_RUN' path and a *.json wildcard to create a glob
+    for getting file paths to .json files in the given directory.
     A random sample up to either the 'MAX_FILE_COUNT' or the max number of json files
-    available in the given directory will be used. The body_text information is then extracted."""
-    # A list for holding the necessary data from each .json file
-    document_list = []
-    # Get system directory from given relative path
+    available in the given directory will be returned."""
+
+     # Get system directory from given relative path
     cord_comm_use_subset_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), DIR_TO_READ)
     # Use directory and wildcard to return glob object of .json file paths
     json_glob = glob.glob(cord_comm_use_subset_dir + "*.json")
@@ -29,13 +23,22 @@ def read_json_files():
     max_file_count = MAX_FILE_COUNT if len(json_glob) >= MAX_FILE_COUNT else len(json_glob)
     # Get a random sample of .json file paths
     json_paths = random.sample(json_glob, max_file_count)
+
+    return json_paths
+
+def get_text_data():
+    """Extracts body text from each json file in the given 'DIR_TO_READ' directory.
+    Returns a list of dictionaries with the file name text data for each file that is read."""
+
+    # A list for holding the necessary data from each .json file
+    document_list = []
+    json_paths = get_json_file_paths()
     
     for json_path in json_paths:
+        # Separate file name and file ext.
+        file_path, file_ext = os.path.splitext(json_path)
+        directory, file_name = os.path.split(file_path)
         try:
-            # Separate file name and file ext.
-            file_path, file_ext = os.path.splitext(json_path)
-            directory, file_name = os.path.split(file_path)
-          
             # Read file by path
             with open(json_path, "r") as f:
                 data = json.load(f)
@@ -51,14 +54,17 @@ def read_json_files():
                     # Append file data to document_list
                     document_list.append(paragraph_dict)
                 else:
-                    print("No data found for {file_name}{file_ext}")
+                    print(f"No data found for {file_name}{file_ext}")
                     continue
         except Exception as ex:
+            print(f"Could not read or extract data from {file_name}{file_ext}")
             print(ex)
+    return document_list
 
 
 def main():
-    read_json_files()
+    document_data = get_text_data()
+    x = 0
 
 
 if __name__ == "__main__":
